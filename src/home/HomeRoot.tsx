35ba262ac9c7b4fbfe6a70c1a69780707bdee6ee
Luke Millar <luketmillar@gmail.com>
2021-03-20 15:33:24 -0700

    Home buttons

--- src/home/HomeRoot.tsx
+++ src/home/HomeRoot.tsx
@@ -1,9 +1,19 @@
+import { Button, Input } from 'components'
 import React from 'react'
 
 const Home = () => {
-    return <div>
-        <div style={{ height: 200 }} />
+    return <div style={{ padding: 24 }}>
         <h1>Pollisa</h1>
+        <div style={{ height: 200 }} />
+        <Button style={{ width: '100%' }} variant='primary'>Create a poll</Button>
+        <div style={{ height: 24 }} />
+        <div style={{ textAlign: 'center' }}>or</div>
+        <div style={{ height: 24 }} />
+        <div style={{ display: 'flex' }}>
+            <Input style={{ width: 190 }} placeholder="Code" />
+            <div style={{ width: 4, flexShrink: 0 }} />
+            <Button style={{ flexShrink: 0 }} disabled variant="primary">Join</Button>
+        </div>
     </div>
 }
 
