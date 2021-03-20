1af6dabf6d84af14681df63f0180c465f5619c21
Luke Millar <luketmillar@gmail.com>
2021-03-20 11:12:07 -0700

    Renames

---
+++ src/home/HomeRoot.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => {
+    return <div>
+        <div style={{ height: 200 }} />
+        <h1>Pollisa</h1>
+    </div>
+}
+
+export default Home
--- src/poll/index.ts
+++ src/poll/index.ts
@@ -1,2 +1,2 @@
-export { default as Root } from './Root'
+export { default as Root } from './PollRoot'
 export * as Questions from './Question'
