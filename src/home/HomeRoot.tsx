1fc6413f15536f856d3e3a7ea36560fe7fce10d3
Luke Millar <luketmillar@gmail.com>
2021-03-21 01:28:32 -0700

    Input layout styles on home

--- src/home/HomeRoot.tsx
+++ src/home/HomeRoot.tsx
@@ -10,10 +10,10 @@ const Home = () => {
         <div style={{ height: 24 }} />
         <div style={{ textAlign: 'center' }}>or</div>
         <div style={{ height: 24 }} />
-        <div style={{ display: 'flex' }}>
-            <Input style={{ width: 190 }} placeholder="Code" />
-            <div style={{ width: 4, flexShrink: 0 }} />
-            <Button style={{ flexShrink: 0 }} disabled variant="primary">Join</Button>
+        <div style={{ display: 'flex', width: '100%' }}>
+            <Input style={{ flex: 1 }} placeholder="Code" />
+            <div style={{ width: 2 }} />
+            <Button disabled variant="primary">Join</Button>
         </div>
     </div>
 }
